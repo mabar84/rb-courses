2545c93708d312a4ddbfd8a191db8aea97ae58fc
HUAWEI <mabar@tut.by>
2023-05-26 00:23:33 +0300

    sort js added

--- src/js/courses-page.js
+++ src/js/courses-page.js
@@ -44,3 +44,68 @@ showMore?.forEach((item) => {
 		item.classList.add("collapsed");
 	});
 });
+
+//sort
+const sort = document.getElementById("sort");
+const sortWrapper = document.querySelectorAll(".sort-wrapper");
+
+const sortItems = document.querySelectorAll(".sort-item");
+const sortItemActive = document.querySelectorAll(".sort-item.active");
+const sortby = document.querySelector(".sortby");
+
+if (sortby) {
+	sortby.textContent = sortItemActive[0].textContent;
+}
+
+sortItems?.forEach((item) => {
+	item.addEventListener("click", (e) => {
+		e.preventDefault();
+
+		sortItems?.forEach((el) => {
+			el.classList.remove("active");
+		});
+		item.classList.add("active");
+		const sortItemActive = document.querySelectorAll(".sort-item.active");
+		sortby.textContent = sortItemActive[0].textContent;
+
+		item.closest(".sort-wrapper").classList.remove("active");
+	});
+});
+
+console.log(sort);
+sort?.addEventListener("click", () => {
+	sort.closest(".sort-wrapper").classList.toggle("active");
+});
+
+//modal
+//add comment
+const addCommentAll = document.querySelectorAll(".add-comment");
+
+addCommentAll?.forEach((item) => {
+	item.addEventListener("click", () => {
+		document.getElementById("modal-comment")?.classList.remove("collapsed");
+		document.body.style.overflow = "hidden";
+	});
+});
+//add review
+document.getElementById("add-review")?.addEventListener("click", () => {
+	document.getElementById("modal-review")?.classList.remove("collapsed");
+	document.body.style.overflow = "hidden";
+});
+
+//close modal
+const modalBgAll = document.querySelectorAll(".modal-bg");
+const modalCloseAll = document.querySelectorAll(".modal-close");
+
+modalBgAll?.forEach((item) => {
+	item.addEventListener("click", () => {
+		item.closest(".modal").classList.add("collapsed");
+		document.body.style.overflow = "auto";
+	});
+});
+modalCloseAll?.forEach((item) => {
+	item.addEventListener("click", () => {
+		item.closest(".modal").classList.add("collapsed");
+		document.body.style.overflow = "auto";
+	});
+});
